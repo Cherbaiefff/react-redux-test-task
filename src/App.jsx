003895ae78866e3077fdb46be8675c362f931a1b
Oleg <cherbaieff@gmail.com>
2023-02-12 18:27:07 +0200

    remove modals from app, move container into products component

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,13 @@
 import React from 'react';
-import Container from './components/UI/Container';
 import Header from './components/Header';
 import Products from './components/Products';
 import Footer from './components/Footer/Footer';
 
-import { useSelector } from 'react-redux';
-
-import SuccessModal from './components/UI/SuccessModal';
-import PurchaseModal from './components/UI/PurchaseModal';
-
 const App = () => {
-    const userData = useSelector((state) => state.userCart);
-
     return (
         <div className="App">
             <Header />
-            <Container>
-                {userData.isPurchaseModalOpen && <PurchaseModal />}
-                {userData.isBought && <SuccessModal />}
-                <Products />
-            </Container>
+            <Products />
             <Footer />
         </div>
     );
